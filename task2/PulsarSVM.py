bf1c32ff216aed40cbb5d1f90ff475a3ec091602
Zenoss <kumaraayan763@gmail.com>
2026-02-15 01:47:59 +0530

    Update PulsarSVM.py

--- task2/PulsarSVM.py
+++ task2/PulsarSVM.py
@@ -1,36 +1,22 @@
 import pandas as pd
-
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-from sklearn.metrics import classification_report, accuracy_score
-
-
-df = pd.read_csv("pulsar_data_test.csv")
-
-
-X = df.iloc[:, :-1]  
-y = df.iloc[:, -1]    
-
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42, stratify=y
-)
-
-
+from sklearn.preprocessing import StandardScaler
+df = pd.read_csv('pulsar_data_test.csv')
+df = df.fillna(df.mean())
+x = df.drop(columns='target_class')
+#target_class was empty so filled y with 0 and 1 acc to condition(Excess kurtosis of the integrated profile>0.7)
+y = (x.iloc[:,2] > 0.7).astype(int)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+#Scaling the data cuz SVM is very sensitive to distance between two data points 
 scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
-linear_svm = SVC(kernel='linear', C=1)
-linear_svm.fit(X_train, y_train)
-
-y_pred_linear = linear_svm.predict(X_test)
-print("Linear SVM Results")
-print("Accuracy:", accuracy_score(y_test, y_pred_linear))
-print(classification_report(y_test, y_pred_linear))
-rbf_svm = SVC(kernel='rbf', C=1, gamma='scale')
-rbf_svm.fit(X_train, y_train)
-y_pred_rbf = rbf_svm.predict(X_test)
-print("\nRBF SVM Results")
-print("Accuracy:", accuracy_score(y_test, y_pred_rbf))
-print(classification_report(y_test, y_pred_rbf))
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+print("Linear kernel: ")
+svm_linear = SVC(kernel='linear')
+svm_linear.fit(x_train, y_train)
+print("Linear Accuracy:", svm_linear.score(x_test, y_test))
+print("Radial Bias Function kernel: ")
+svm_rbf = SVC(kernel='rbf', gamma='scale')
+svm_rbf.fit(x_train, y_train)
+print("RBF Accuracy:", svm_rbf.score(x_test, y_test))
